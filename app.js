7981ee3fff005e16dead8fc9074e7afcec04cfc7
Pavan-Bathieni <S566651@nwmissouri.edu>
2023-10-25 11:35:31 -0500

    new endpoint

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mydata', mydataRouter);
-app.use('/computation', computationomputation);
+app.use('/computation', computation);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
