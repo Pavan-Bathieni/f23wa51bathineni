da6408b0b5c64862368ad51a9b295c39b1e52136
Pavan-Bathieni <S566651@nwmissouri.edu>
2023-10-25 14:41:48 -0500

    computation

--- routes/computation.js
+++ routes/computation.js
@@ -3,11 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+    r=Math.random();
   log=Math.log(r)
   res.send(`Log function is applied on ${r} is ${log}`);
 });
 
 module.exports = router;
 
-var r=Math.random();
+var r=0;
 var log=0;
